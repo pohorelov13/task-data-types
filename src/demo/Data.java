662e42013f43986837aca3269cfa2e05f3d7e90f
Bohdan Pohorelov <pogorelov.play@gmail.com>
2022-11-10 09:36:14 +0200

    Some improvements

--- src/demo/Data.java
+++ src/demo/Data.java
@@ -6,13 +6,13 @@ public class Data {
         Tasks task = new Tasks();
 
         task.printThreeWords();
-        task.checkSumSign();
-        task.printColor();
-        task.compareNumbers();
-        System.out.println(task.checkSum(5, 12));
-        task.checkSign(-15);
-        System.out.println(task.checkPositive(33));
+        task.checkSumSign(); //5, 7 - ожидается "Sum is positive"
+        task.printColor(); //65 - ожидается "YELLOW"
+        task.compareNumbers(); //3, 12 - ожидается "a < b"
+        System.out.println(task.checkSum(5, 12)); //ожидается - true
+        task.checkSign(-15);//ожидается - "-15 is negative"
+        System.out.println(task.checkPositive(33));//ожидается - false
         task.printInfo("Better call Saul!", 5);
-        System.out.println(task.checkYear(2022));
+        System.out.println(task.checkYear(2022));//ожидается - false
     }
 }
